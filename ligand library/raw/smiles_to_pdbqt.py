6c67d404fa26f35ccf9db5562fc7524072b78baa
Ladislav Buček <ladislavbucek@Ladislavs-MacBook-Air.local>
2025-01-05 01:47:24 +0100

    smiles_to_pdbqt changed to init version, generated valid pdbqt files

--- ligand library/raw/smiles_to_pdbqt.py
+++ ligand library/raw/smiles_to_pdbqt.py
@@ -21,34 +21,16 @@ if 'Smiles' not in data.columns:
 total_molecules = len(data)
 successful_conversions = 0
 failed_conversions = []
-optimization_steps = 1000  # Adjust based on molecule complexity
-force_field = "MMFF94"  # or "UFF" if necessary
 
 # Iterate through the rows
 for index, row in data.iterrows():
     smiles = row['Smiles']
     molecule_id = row.get('ChEMBL ID', f"mol_{index+1}")  # Default ID if not provided
+    output_pdbqt = os.path.join(output_dir, f"{molecule_id}.pdbqt")
     
-    # Define file paths
-    initial_pdb = os.path.join(output_dir, f"{molecule_id}_initial.pdb")
-    optimized_pdb = os.path.join(output_dir, f"{molecule_id}_optimized.pdb")
-    pdbqt_file = os.path.join(output_dir, f"{molecule_id}.pdbqt")
-
-    # Step 1: Generate 3D coordinates with hydrogens
-    os.system(f'obabel -:"{smiles}" -O {initial_pdb} --gen3d -p')
-
-
-    # Step 2: Optimize geometry
-    os.system(f'obminimize -ff {force_field} -n {optimization_steps} {initial_pdb} -O {optimized_pdb}')
-
-    # Step 3: Convert optimized PDB to PDBQT
-    result = os.system(f'obabel {optimized_pdb} -O {pdbqt_file}')
+    # Run OpenBabel to convert SMILES to PDBQT
+    result = os.system(f'obabel -:"{smiles}" -O {output_pdbqt} --gen3d -p')
     
-    os.remove(initial_pdb)
-    #os.remove(optimized_pdb)
-
-    print(f"Processed {molecule_id}: {pdbqt_file}")
-
     # Check if the conversion was successful
     if result == 0:  # OpenBabel returns 0 for success
         successful_conversions += 1
